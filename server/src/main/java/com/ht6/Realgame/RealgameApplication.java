9afc336495b6c35c480bbfca42b5a6d19e4839eb
GabrielGenkov <gabriel.genkov2002@gmail.com>
2020-10-02 15:43:31 +0300

    Set Spring Boot

---
+++ server/src/main/java/com/ht6/Realgame/RealgameApplication.java
@@ -0,0 +1,13 @@
+package com.ht6.Realgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealgameApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ht6/Realgame/RealgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ht6.Realgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
